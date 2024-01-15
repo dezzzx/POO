a622a5dccd9cb38c8bf6b060a25504f61460fb22
AngelMenenedezDAM1 <150004040+AngelMenenedezDAM1@users.noreply.github.com>
2024-01-15 12:23:56 +0100

    Ejemplo_01 orientado a objetos

---
+++ POO/src/packagepoo/coche.java
@@ -0,0 +1,19 @@
+package packagepoo;
+
+public class coche {	
+		int ruedas;
+		int ruedas;
+		int largo;
+		int ancho;
+		int motor;
+		int peso;
+
+	public coche() {
+		ancho=250;
+		largo=400;
+		ruedas=4;
+		motor=250;
+		peso=1900;
+}
+	
+}
---
+++ POO/src/packagepoo/uso_coche.java
@@ -0,0 +1,11 @@
+package packagepoo;
+
+public class uso_coche {
+
+	public static void main(String[] args) {
+		coche Mercedes = new coche();
+		System.out.println("Mi coche tiene "+Mercedes.ruedas+" ruedas y tiene "+Mercedes.motor+" CV.");
+
+	} 
+
+}
