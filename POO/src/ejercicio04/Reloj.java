f1c176b41323749c912034b95c615fb759745d96
AngelMenenedezDAM1 <150004040+AngelMenenedezDAM1@users.noreply.github.com>
2024-01-15 19:10:16 +0100

    ejercicio 4

--- POO/src/ejercicio04/Reloj.java
+++ POO/src/ejercicio04/Reloj.java
@@ -3,12 +3,11 @@ package ejercicio04;
 public class Reloj {
 	String marca;
 	boolean digital;
-	
 	double precio;
+	
 	public Reloj() {
 		marca="Casio G-shock";
 		digital=false;
-		
 		precio=299.00;
 	}
 	  public String Tipo() {
